b3ed50695d1304417ff4cf9a75321060087ae1e3
Jair Avilés <hi@jairaviles.mx>
2017-12-03 19:38:35 -0200

    refactor styles applying BEM methodology

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -7,7 +7,11 @@ export default  class PlayerList extends React.Component {
     renderPlayers() {
 
         if (this.props.players.length === 0) {
-            return <p>No players recorded. add your first player to get started.</p>
+            return (
+                <div className="item">
+                    <p className="item__message">No players recorded. add your first player to get started.</p>
+                </div>
+            )
         } else {
             return this.props.players.map((player) => {
                 return <Player key={player._id} player={player}/>;
